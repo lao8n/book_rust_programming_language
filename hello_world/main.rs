4fbfd29fb8a0178ef41dbad93f0644e87c68989c
Nicholas Drake <hellolao8n@gmail.com>
2024-06-03 18:02:18 +0100

    hello world and hello cargo

---
+++ hello_world/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!"); // ! calls a rust macro
+}
+
+// compile with rustc main.rs
+// run with .\main.exe
+// .pdb file is created for debugging only on windows
+
+// rust is ahead-of-time meaning it compiles to machine code and doesn't require rust to be installed to run
