ef4def79c7ab4b3e8b7cfdb540aa7f4cf2deb875
Nicholas Drake <hellolao8n@gmail.com>
2024-06-17 12:14:18 +0100

    started grep project

---
+++ ch_12_project/minigrep/src/main.rs
@@ -0,0 +1,14 @@
+use std::env; // std:env::arg_os if need non unicode characters
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect(); // collect makes iterator into vector
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
+}
---
+++ ch_12_project/minigrep/target/debug/deps/minigrep.d
@@ -0,0 +1,5 @@
+C:\Users\lao8n\OneDrive\Documents\book_rust_programming_language\ch_12_project\minigrep\target\debug\deps\minigrep.exe: src\main.rs
+
+C:\Users\lao8n\OneDrive\Documents\book_rust_programming_language\ch_12_project\minigrep\target\debug\deps\minigrep.d: src\main.rs
+
+src\main.rs:
