30fd12164ea0f645abf3e0904123f780a085354a
Nicholas Drake <hellolao8n@gmail.com>
2024-06-17 12:56:45 +0100

    refactored into Config struct

--- ch_12_project/minigrep/src/main.rs
+++ ch_12_project/minigrep/src/main.rs
@@ -3,12 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // collect makes iterator into vector
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    let config = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
