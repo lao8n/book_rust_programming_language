bc49fa139b313b8c9edf9560a9cf3a53a6cc76de
Nicholas Drake <hellolao8n@gmail.com>
2024-06-17 13:08:47 +0100

    return a Result rather than panicking when there is an error

--- ch_12_project/minigrep/src/main.rs
+++ ch_12_project/minigrep/src/main.rs
@@ -1,9 +1,13 @@
 use std::env; // std:env::arg_os if need non unicode characters
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); // collect makes iterator into vector
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| { // closure
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
@@ -17,9 +21,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    // programmers expect new to never fail
+    fn build(args: &[String]) -> Result<Config, &'static str> { // errors should alwasy be string literals that have static lifetime
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Ok(Config { query, file_path })
+    }
 }
